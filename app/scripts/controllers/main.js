c20636bd9bc17920f661c94a3eb14a057cf77108
Daniel Schmidt <dschmidt@weluse.de>
2013-05-17 19:27:25 +0200

    made lint conformer

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,29 +12,29 @@ angular.module('canIUseMyCssApp')
 
     $scope.isProperty = function(element){
       // Only a property if it has a : doesnt has a } or . or # or / or ' *'
-      return ((element.indexOf(":") !== -1)
-           && (element.indexOf("}") === -1)
-           && (element.indexOf(".") !== 0)
-           && (element.indexOf("#") !== 0)
-           && (element.indexOf("/") !== 0)
-           && (element.indexOf("*") === -1)
-           && (element.indexOf("-") === -1));
+      return ((element.indexOf(':') !== -1)
+           && (element.indexOf('}') === -1)
+           && (element.indexOf('.') !== 0)
+           && (element.indexOf('#') !== 0)
+           && (element.indexOf('/') !== 0)
+           && (element.indexOf('*') === -1)
+           && (element.indexOf('-') === -1));
     }
     $scope.addCSS = function(){
       //TODO: Refactor with underscore js
       var clear_properties = [];
-      _.each($scope.cssCode.split("{"), function(selector){
-        _.each(selector.split(";"), function(property){
+      _.each($scope.cssCode.split('{'), function(selector){
+        _.each(selector.split(';'), function(property){
           var property = property.trim();
           // go through all properties
           if($scope.isProperty(property)){
-            clear_properties.push(property.split(":")[0].trim());
+            clear_properties.push(property.split(':')[0].trim());
           }
         });
       });
       var unsorted_properties = _.uniq(clear_properties);
       $scope.properties = _.sortBy(unsorted_properties, function (name) {return name});
-      console.info("All the properties are: " + $scope.properties);
+      console.info('All the properties are: ' + $scope.properties);
       $scope.active = $scope.properties[0];
       $scope.search_done = true;
     };
